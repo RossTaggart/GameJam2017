cf4022b54834dcee5dcca1de7e5924592246c382
ckinne1771 <ckinney1771@gmail.com>
2017-01-21 16:34:58 +0000

    EnemyPatrolsAdded
    Enemy Feature Ready For merge

---
+++ GameJam2017/Assets/scripts/EnemyPatrolScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolScript : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public float patrolSpeed = 5;
+    public float waitTimer = 5.0f;
+
+    public GameObject currentWaypoint;
+    public int currentIndex;
+    public bool isWaiting = false;
+    // Use this for initialization
+    void Start()
+    {
+        currentIndex = 0;
+        currentWaypoint = waypoints[currentIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaiting == false)
+        {
+            
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(currentWaypoint.transform.position.x,
+                this.transform.position.y, currentWaypoint.transform.position.z), (patrolSpeed*Time.deltaTime));
+        }
+
+        if(isWaiting==true)
+        {
+
+
+            waitTimer = waitTimer - (1 * Time.deltaTime);
+            if (waitTimer <= 0.0f)
+            {
+                isWaiting = false;
+                waitTimer = 5.0f;
+            }
+        }
+
+    }
+
+   
+   void OnTriggerEnter(Collider other)
+    {
+        
+
+        if(other.gameObject.tag=="EnemyWaypoint")
+        {
+            currentIndex++;
+            if((currentIndex-1) >= waypoints.Length -1)
+            {
+                currentIndex = 0;
+            }
+            isWaiting = true;
+           
+           
+            currentWaypoint = waypoints[currentIndex];
+            SightScript sight = this.GetComponentInChildren<SightScript>();
+            sight.target = currentWaypoint.transform;
+        }
+    }
+}
--- GameJam2017/Assets/scripts/SightScript.cs
+++ GameJam2017/Assets/scripts/SightScript.cs
@@ -17,7 +17,7 @@ public class SightScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         sight.transform.LookAt(new Vector3(target.position.x, sight.position.y, target.position.z));
-        Debug.Log(sight.transform.rotation);
+        
 
         RaycastHit hit;
         if (Physics.Raycast(sight.transform.position, sight.transform.forward, out hit, range))
@@ -25,13 +25,13 @@ public class SightScript : MonoBehaviour {
             if (hit.collider.tag == "Player")
             {
                 canSee = true;
-                Debug.Log(canSee);
+               
             }
             else
             {
 
                 canSee = false;
-                Debug.Log(canSee);
+               
             }
         }
 	}
