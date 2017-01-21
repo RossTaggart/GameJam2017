3228f2a064835b12d4f5d8e333591e51e1fd3855
ckinne1771 <ckinney1771@gmail.com>
2017-01-21 13:51:24 +0000

    Enemy Raycasting working as intended.
    Enemies also rotate to look towards next waypoint TODO: make patrols
    between waypoints

---
+++ GameJam2017/Assets/scripts/SightScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightScript : MonoBehaviour {
+
+    public float range = 100;
+    public Transform target;
+    public Transform sight;
+
+    private bool canSee;
+	// Use this for initialization
+	void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        sight.transform.LookAt(new Vector3(target.position.x, sight.position.y, target.position.z));
+        Debug.Log(sight.transform.rotation);
+
+        RaycastHit hit;
+        if (Physics.Raycast(sight.transform.position, sight.transform.forward, out hit, range))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                canSee = true;
+                Debug.Log(canSee);
+            }
+            else
+            {
+
+                canSee = false;
+                Debug.Log(canSee);
+            }
+        }
+	}
+}
