559406e423bd0c64e589db142278afaddd39edf0
crystalix <e.gillespie42@hotmail.com>
2017-01-21 14:41:57 +0000

    basic local networking using unity editor and exe

---
+++ GameJam2017/Assets/scripts/networkScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class networkScript : MonoBehaviour {
+
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
