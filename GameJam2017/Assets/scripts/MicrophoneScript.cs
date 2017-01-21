f9ecda92f98f445d58f0ea632a968e85784acb9a
Alex Malcolm <alex.malcolm@pzflex.com>
2017-01-21 01:17:46 +0000

    added basic scene to start prototyping the level for hamish and attempted the microphone script - dicking up somewhere will have a look tomorrow!

---
+++ GameJam2017/Assets/scripts/MicrophoneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        string[] devices = Microphone.devices;
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start(devices[0], true, 10, 44100);
+        aud.Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
